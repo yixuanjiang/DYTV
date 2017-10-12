4db324b4c0ca4419535926f63c6f4d6d2cf58ccd
蒋毅轩 <jiangyixuan@jiangyixuandeMacBook-Air-2.local>
2017-10-12 14:57:01 +0800

    1.创建项目

---
+++ DOZB/DOZB/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DOZB
+//
+//  Created by 蒋毅轩 on 2017/10/12.
+//  Copyright © 2017年 蒋毅轩. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DOZB/DOZB/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DOZB
+//
+//  Created by 蒋毅轩 on 2017/10/12.
+//  Copyright © 2017年 蒋毅轩. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
